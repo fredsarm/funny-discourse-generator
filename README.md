0378d9b3b6c515942675fbee1153eb3110320495
fredsarm <fredsarm.js@gmail.com>
2022-08-01 18:59:10 -0300

    First commit

---
+++ README.md
@@ -0,0 +1,49 @@
+# Funny Discourse Generator
+Have fun generating politician empty and abstract discourses runing this software in a terminal.
+## Table of Contents
+* [General Info](#general-information)
+* [Technologies Used](#technologies-used)
+* [Setup](#setup)
+* [Usage](#usage)
+* [Project Status](#project-status)
+* [Room for Improvement](#room-for-improvement)
+* [Contact](#contact)
+<!-- * [License](#license) -->
+
+
+## General Information
+- This is a portifolio project that concludes the JavaScript Syntax molule of the Codecademy Full Stack Engeneering path.
+- Its main function is to prepare randomized discourses with extremely abstract expressions that fit up in almost all situations.
+- The purpose of this project is to have fun imagining how some politicians mock people with empty speeches.
+- I've undertaken it in the afternoon of 8 january 2022. That's a very simple code in which I code down a funny game I used to play when I was younger. We had a sheet containing the fragments of politicians discourses and each player had to compose some phrase and say it loud.
+
+
+## Technologies Used
+- Programming Language: JavaScript
+- Node v10.19.0.
+- VSCode Version 1.63.2
+- Git Version 19.12.3 syncing GitHub.
+- Linux kubuntu Version 20.04
+- Konsole terminal Version 19.12.3
+
+## Setup
+You need to have node installed in your machine.
+
+## Usage
+Save main.js in a directory and run "node main.js" (without quotes).
+
+`$ node main.js`
+
+## Project Status
+Project is: _complete_
+
+## Room for Improvement (TODO)
+- Develop and implement a front end interface to provide access to common users.
+- Create a postgresq database to store much more text fragments. Conect the page to the database.
+- Create parameters to enter the name of the politician, some concrete stuff to substitute words in text and become the game more interesting.
+
+
+## Contact
+Created by [Frederico Sarmento](https://www.linkedin.com/in/fredsarm) - feel free to contact me!
+
+This project is open source. Use it with no limitations.
---
+++ main.js
@@ -0,0 +1,87 @@
+//Declare arrays with the fragments of text.
+const fragment1 = [
+    'Dear people of my country, ',
+    'We should never forget that ',
+    'The experience shows us that ',
+    'It\'s not too much to insist that ',
+    'I think that it\'s very important to emphasize that ',
+    'The encouragement in the technological progress as well as ',
+    'If we ponder, we see that ',
+    'Today, ',
+    'In the context of the current policy, ',
+    'All data points out that ',
+    'As a result, ',
+    'This awesome project makes me believe that '
+];
+const fragment2 = [
+    'our effort to accomplish this challenge ',
+    'a great complexity of studies ',
+    'the new structural model, recommended here, ',
+    'the expansion of our activity ',
+    'the assess of those results ',
+    'the development of different ways of acting '
+];
+const fragment3 = [
+    'compels us to the analyses ',
+    'plays a fundamental role in the formulation ',
+    'requires precision and definition ',
+    'offers an excellent opportunity for the verification ',
+    'takes position in the configuration ',
+    'gives focus to the importance '
+];
+const fragment4 = [
+    'of our financial and administrative goals.',
+    'of basic politics toward the success of the program.',
+    'of specific conditions for each business.',
+    'of our team building potential.',
+    'of the government attributions.',
+    'of the procedures.'
+];
+
+//Declare randomized fragments arrays
+let seq1 = [];
+let seq2 = [];
+let seq3 = [];
+let seq4 = [];
+
+//Declare the string for the output of the discourse
+let discourseString = '';
+
+//Generates a random id
+function randId(numElements) {
+    return Math.floor(Math.random() * (numElements));
+}
+
+//Populate the randomized arrays.
+do {
+    let currRandId = randId(12);
+    if (!seq1.includes(currRandId)) {
+        seq1.push(currRandId);
+    }
+} while (seq1.length <= 5);
+do {
+    let currRandId = randId(6);
+    if (!seq2.includes(currRandId)) {
+        seq2.push(currRandId);
+    }
+} while (seq2.length <= 5);
+do {
+    let currRandId = randId(6);
+    if (!seq3.includes(currRandId)) {
+        seq3.push(currRandId);
+    }
+} while (seq3.length <= 5);
+do {
+    let currRandId = randId(6);
+    if (!seq4.includes(currRandId)) {
+        seq4.push(currRandId);
+    }
+} while (seq4.length <= 5);
+
+//Create the string with randomized array elements
+for (let i = 0; i <= 5; i++) {
+    discourseString = discourseString + fragment1[seq1[i]] + fragment2[seq2[i]] + fragment3[seq3[i]] + fragment4[seq4[i]] + '\n'
+}
+
+//log the discourse
+console.log(discourseString);
